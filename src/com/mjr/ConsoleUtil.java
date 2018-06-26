99deab7a58eb239d6c4a4f81aa0f8ab9578c5947
MJRLegends <MJRLegends@users.noreply.github.com>
2018-06-26 03:46:25 +0100

    Tweaks to Prefixs in Console messages

--- src/com/mjr/ConsoleUtil.java
+++ src/com/mjr/ConsoleUtil.java
@@ -8,23 +8,29 @@ public class ConsoleUtil {
 
     public static void TextToConsole(String message, String MessageType, String sender) {
 	TextToConsole(null, null, message, MessageType, sender);
-   }
+    }
 
     public static void TextToConsole(BotType type, String channel, String message, String MessageType, String sender) {
 	if (MessageType == "Chat") {
 	    if (sender != null) {
 		if (Permissions.hasPermission(type, channel, sender, PermissionLevel.Bot.getName())) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " - [Channel]" + channel +" - [Bot] " + sender + ": " + message);
+		    System.out.println(
+			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Bot]" + sender + ": " + message);
 		} else if (type == BotType.Twitch && sender.endsWith(channel)) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " - [Channel]\" + channel +\" - [Streamer] " + sender + ": " + message);
+		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Streamer]" + sender
+			    + ": " + message);
 		} else if (type == BotType.Mixer && sender.equalsIgnoreCase(channel)) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " - [Channel]\" + channel +\" - [Streamer] " + sender + ": " + message);
+		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Streamer]" + sender
+			    + ": " + message);
 		} else if (Permissions.hasPermission(type, channel, sender, PermissionLevel.BotOwner.getName())) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " - [Channel]\" + channel +\" - [Bot Owner] " + sender + ": " + message);
+		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Bot Owner]" + sender
+			    + ": " + message);
 		} else if (Permissions.hasPermission(type, channel, sender, PermissionLevel.Moderator.getName())) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " - [Channel]\" + channel +\" - [Moderator] " + sender + ": " + message);
+		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " - [Moderator]" + sender
+			    + ": " + message);
 		} else if (Permissions.hasPermission(type, channel, sender, PermissionLevel.User.getName())) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " - [Channel]\" + channel +\" - [User] " + sender + ": " + message);
+		    System.out.println(
+			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " - [User]" + sender + ": " + message);
 		}
 	    }
 	} else {
