ee16816bf22ecd9dd596ef6920efb71bb8d07654
MJRLegends <MJRLegends@users.noreply.github.com>
2018-06-29 02:26:08 +0100

    Released v1.2.4 - Beta

--- src/com/mjr/MJRBot.java
+++ src/com/mjr/MJRBot.java
@@ -16,7 +16,7 @@ import com.mjr.threads.ChannelListUpdateThread;
 import com.mjr.threads.PointsThread;
 
 public class MJRBot {
-    public static final String VERSION = "1.2.3 - Beta, Server Version";
+    public static final String VERSION = "1.2.4 - Beta, Server Version";
 
     public static String filePath;
 
