6feb740daa5bc9d923f5fca0b55be24feab21c16
MJRLegends <MJRLegends@users.noreply.github.com>
2018-06-29 02:25:11 +0100

    Small Improvement to the efficiency of the bot

--- src/com/mjr/ConsoleUtil.java
+++ src/com/mjr/ConsoleUtil.java
@@ -17,45 +17,47 @@ public class ConsoleUtil {
 		    System.out.println(
 			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Bot]" + sender + ": " + message);
 		} else if (type == BotType.Twitch && sender.endsWith(channel)) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Streamer]" + sender
-			    + ": " + message);
+		    System.out.println(
+			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Streamer]" + sender + ": " + message);
 		} else if (type == BotType.Mixer && sender.equalsIgnoreCase(channel)) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Streamer]" + sender
-			    + ": " + message);
+		    System.out.println(
+			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Streamer]" + sender + ": " + message);
 		} else if (Permissions.hasPermission(type, channel, sender, PermissionLevel.BotOwner.getName())) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Bot Owner]" + sender
-			    + ": " + message);
+		    System.out.println(
+			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " [Bot Owner]" + sender + ": " + message);
 		} else if (Permissions.hasPermission(type, channel, sender, PermissionLevel.Moderator.getName())) {
-		    System.out.println("\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " - [Moderator]" + sender
-			    + ": " + message);
+		    System.out.println(
+			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " - [Moderator]" + sender + ": " + message);
 		} else if (Permissions.hasPermission(type, channel, sender, PermissionLevel.User.getName())) {
 		    System.out.println(
 			    "\n" + "[Bot Type]" + type.getTypeName() + " [Channel]" + channel + " - [User]" + sender + ": " + message);
 		}
 	    }
 	} else {
-	    System.out.println("[MJRBot Info] " + "[Bot Type]" + type.getTypeName() + channel == null ? "" : ("[Channel] " + channel) + " - " + message);
+	    System.out.println("[MJRBot Info] " + "[Bot Type]" + type.getTypeName() + channel == null ? ""
+		    : ("[Channel] " + channel) + " - " + message);
 	}
     }
 
     public void LeaveChannel(BotType type, String channelName) {
 	if (Config.getSetting("SilentJoin").equalsIgnoreCase("false")) {
-	    if (type == BotType.Twitch)
-		if (MJRBot.getTwitchBotByChannelName(channelName).isConnected())
-		    MJRBot.getTwitchBotByChannelName(channelName)
-			    .MessageToChat(MJRBot.getTwitchBotByChannelName(channelName).getNick() + " Disconnected!");
-		else if (MJRBot.getMixerBotByChannelName(channelName).isConnected()
-			& MJRBot.getMixerBotByChannelName(channelName).isAuthenticated())
-		    MJRBot.getMixerBotByChannelName(channelName)
-			    .sendMessage(MJRBot.getMixerBotByChannelName(channelName).getBotName() + " Disconnected!");
+	    if (type == BotType.Twitch) {
+		TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
+		if (twitchBot.isConnected())
+		    twitchBot.MessageToChat(twitchBot.getNick() + " Disconnected!");
+	    } else if (type == BotType.Mixer) {
+		MixerBot mixerBot = MJRBot.getMixerBotByChannelName(channelName);
+		if (mixerBot.isConnected() & mixerBot.isAuthenticated())
+		    mixerBot.sendMessage(MJRBot.getMixerBotByChannelName(channelName).getBotName() + " Disconnected!");
+	    }
 	}
 	if (type == BotType.Twitch) {
-	    MJRBot.getTwitchBotByChannelName(channelName).partChannel(MJRBot.getTwitchBotByChannelName(channelName).getChannel());
-	    TextToConsole(type, channelName, "Left " + MJRBot.getTwitchBotByChannelName(channelName).getChannel() + " channel", "Bot",
-		    null);
-	    MJRBot.getTwitchBotByChannelName(channelName).viewers = new String[0];
-	    MJRBot.getTwitchBotByChannelName(channelName).ConnectedToChannel = false;
-	    MJRBot.getTwitchBotByChannelName(channelName).setChannel("");
+	    TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
+	    twitchBot.partChannel(twitchBot.getChannel());
+	    TextToConsole(type, channelName, "Left " + twitchBot.getChannel() + " channel", "Bot", null);
+	    twitchBot.viewers = new String[0];
+	    twitchBot.ConnectedToChannel = false;
+	    twitchBot.setChannel("");
 	} else {
 	    MJRBot.getMixerBotByChannelName(channelName).disconnect();
 	    TextToConsole(type, channelName, "Left " + channelName + " channel", "Bot", null);
@@ -63,9 +65,10 @@ public class ConsoleUtil {
     }
 
     public void JoinChannel(BotType type, String channelName) {
-	MJRBot.getTwitchBotByChannelName(channelName).setChannel(channelName);
-	TextToConsole(type, channelName, "Joined " + MJRBot.getTwitchBotByChannelName(channelName).getChannel()
-		.substring(MJRBot.getTwitchBotByChannelName(channelName).getChannel().indexOf("#") + 1) + " channel", "Bot", null);
-	MJRBot.getTwitchBotByChannelName(channelName).joinChannel(MJRBot.getTwitchBotByChannelName(channelName).getChannel());
+	TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
+	twitchBot.setChannel(channelName);
+	TextToConsole(type, channelName, "Joined " + twitchBot.getChannel().substring(twitchBot.getChannel().indexOf("#") + 1) + " channel",
+		"Bot", null);
+	twitchBot.joinChannel(twitchBot.getChannel());
     }
 }
--- src/com/mjr/commands/defaultCommands/DisconnectCommand.java
+++ src/com/mjr/commands/defaultCommands/DisconnectCommand.java
@@ -1,6 +1,8 @@
 package com.mjr.commands.defaultCommands;
 
 import com.mjr.MJRBot;
+import com.mjr.MixerBot;
+import com.mjr.TwitchBot;
 import com.mjr.MJRBot.BotType;
 import com.mjr.Permissions.PermissionLevel;
 import com.mjr.commands.Command;
@@ -10,11 +12,13 @@ public class DisconnectCommand extends Command{
     @Override
     public void onCommand(BotType type, Object bot, String channel, String sender, String login, String hostname, String message, String[] args) {
 	if (type == BotType.Twitch) { // TODO Fix this
-	    MJRBot.getTwitchBotByChannelName(channel).MessageToChat(MJRBot.getTwitchBotByChannelName(channel).getBotName() + " Disconnected!");
-	    MJRBot.getTwitchBotByChannelName(channel).disconnectTwitch();
+	    TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channel);
+	    twitchBot.MessageToChat(twitchBot.getBotName() + " Disconnected!");
+	    twitchBot.disconnectTwitch();
 	} else if (type == BotType.Mixer) {
-	    MJRBot.getMixerBotByChannelName(channel).sendMessage(MJRBot.getMixerBotByChannelName(channel).getBotName() + " Disconnected!");
-	    MJRBot.getMixerBotByChannelName(channel).disconnect();
+	    MixerBot mixerBot = MJRBot.getMixerBotByChannelName(channel);
+	    mixerBot.sendMessage(mixerBot.getBotName() + " Disconnected!");
+	    mixerBot.disconnect();
 	}
     }
 
