646b0ae98c3a151b0647b7002af524677b2b872e
MJRLegends <MJRLegends@users.noreply.github.com>
2018-07-01 00:47:31 +0100

    Removed unused code

--- src/com/mjr/ConsoleUtil.java
+++ src/com/mjr/ConsoleUtil.java
@@ -2,7 +2,6 @@ package com.mjr;
 
 import com.mjr.MJRBot.BotType;
 import com.mjr.Permissions.PermissionLevel;
-import com.mjr.files.Config;
 
 public class ConsoleUtil {
 
@@ -37,38 +36,5 @@ public class ConsoleUtil {
 	    System.out.println("[MJRBot Info] " + "[Bot Type] " +(type == null ? "Unknown" : type.getTypeName()) + " [Channel] "
 		    + (channel == "Unknown" ? "" : channel) + " - " + message);
 	}
-    }
-
-    public void LeaveChannel(BotType type, String channelName) {
-	if (Config.getSetting("SilentJoin").equalsIgnoreCase("false")) {
-	    if (type == BotType.Twitch) {
-		TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
-		if (twitchBot.isConnected())
-		    twitchBot.MessageToChat(twitchBot.getNick() + " Disconnected!");
-	    } else if (type == BotType.Mixer) {
-		MixerBot mixerBot = MJRBot.getMixerBotByChannelName(channelName);
-		if (mixerBot.isConnected() & mixerBot.isAuthenticated())
-		    mixerBot.sendMessage(MJRBot.getMixerBotByChannelName(channelName).getBotName() + " Disconnected!");
-	    }
-	}
-	if (type == BotType.Twitch) {
-	    TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
-	    twitchBot.partChannel(twitchBot.getChannel());
-	    TextToConsole(type, channelName, "Left " + twitchBot.getChannel() + " channel", "Bot", null);
-	    twitchBot.viewers = new String[0];
-	    twitchBot.ConnectedToChannel = false;
-	    twitchBot.setChannel("");
-	} else {
-	    MJRBot.getMixerBotByChannelName(channelName).disconnect();
-	    TextToConsole(type, channelName, "Left " + channelName + " channel", "Bot", null);
-	}
-    }
-
-    public void JoinChannel(BotType type, String channelName) {
-	TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
-	twitchBot.setChannel(channelName);
-	TextToConsole(type, channelName, "Joined " + twitchBot.getChannel().substring(twitchBot.getChannel().indexOf("#") + 1) + " channel",
-		"Bot", null);
-	twitchBot.joinChannel(twitchBot.getChannel());
     }
 }
