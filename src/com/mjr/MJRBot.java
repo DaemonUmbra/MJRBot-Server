ef55d2152f2d54a6e5ce3e4655c7cdda54cec269
MJRLegends <MJRLegends@users.noreply.github.com>
2018-07-01 03:15:31 +0100

    Released v1.4.0 - Beta

--- src/com/mjr/MJRBot.java
+++ src/com/mjr/MJRBot.java
@@ -16,7 +16,7 @@ import com.mjr.threads.ChannelListUpdateThread;
 import com.mjr.threads.PointsThread;
 
 public class MJRBot {
-    public static final String VERSION = "1.3.9 - Beta, Server Version";
+    public static final String VERSION = "1.4.0 - Beta, Server Version";
 
     public static String filePath;
 
@@ -56,7 +56,7 @@ public class MJRBot {
 	    String connectionType = "";
 	    do {
 		connectionType = console.readLine("Bot Type: Database or Manual?");
-		// connectionType = "Database";
+		//connectionType = "Database";
 		if (connectionType.equalsIgnoreCase("Manual")) {
 		    do {
 			String botType;
