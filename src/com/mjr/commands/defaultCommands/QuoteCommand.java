0efe8a249519620f2b436d242b288ce66791a3a8
MJRLegends <MJRLegends@users.noreply.github.com>
2018-07-03 17:00:53 +0100

    Moved Quote Command functions into its own class

--- src/com/mjr/commands/defaultCommands/QuoteCommand.java
+++ src/com/mjr/commands/defaultCommands/QuoteCommand.java
@@ -1,17 +1,6 @@
 package com.mjr.commands.defaultCommands;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
 
 import com.mjr.ConsoleUtil;
 import com.mjr.ConsoleUtil.MessageType;
@@ -19,10 +8,10 @@ import com.mjr.MJRBot;
 import com.mjr.MJRBot.BotType;
 import com.mjr.Permissions;
 import com.mjr.Permissions.PermissionLevel;
-import com.mjr.TwitchBot;
 import com.mjr.Utilities;
 import com.mjr.commands.Command;
 import com.mjr.files.Config;
+import com.mjr.files.QuoteFile;
 
 public class QuoteCommand extends Command {
     public static String filename = "Quotes.txt";
@@ -34,45 +23,14 @@ public class QuoteCommand extends Command {
 	if (Config.getSetting("Quotes", channel).equalsIgnoreCase("true")) {
 	    file = new File(MJRBot.filePath + channel + File.separator + filename);
 	    if (args.length == 1) {
-		String token1 = "";
-		Scanner inFile1 = null;
-		try {
-		    inFile1 = new Scanner(file).useDelimiter(";\\s*");
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		}
-		List<String> temps = new ArrayList<String>();
-		while (inFile1.hasNext()) {
-		    token1 = inFile1.next();
-		    temps.add(token1);
-		}
-		inFile1.close();
-		String[] tempsArray = temps.toArray(new String[0]);
-		Random rand = new Random();
-		((TwitchBot) bot).sendMessage(MJRBot.getTwitchBotByChannelName(channel).getChannel(),
-			tempsArray[rand.nextInt(tempsArray.length)]);
+		Utilities.sendMessage(type, channel, QuoteFile.getRandomQuote(channel, file));
 	    } else if (args.length == 3 && args[1].equalsIgnoreCase("get")
 		    && Permissions.hasPermission(bot, type, channel, sender, PermissionLevel.Moderator.getName())) {
-		String token1 = "";
-		Scanner inFile1 = null;
-		try {
-		    inFile1 = new Scanner(file).useDelimiter(";\\s*");
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		}
-		List<String> temps = new ArrayList<String>();
-		while (inFile1.hasNext()) {
-		    token1 = inFile1.next();
-		    temps.add(token1);
-		}
-		inFile1.close();
-		String[] tempsArray = temps.toArray(new String[0]);
-		if (Integer.parseInt(args[2]) < tempsArray.length)
-		    ((TwitchBot) bot).sendMessage(MJRBot.getTwitchBotByChannelName(channel).getChannel(),
-			    "Quote " + args[2] + " is: " + tempsArray[Integer.parseInt(args[2])]);
+		String quote = QuoteFile.getQuote(channel, file, Integer.parseInt(args[2]));
+		if (quote != null)
+		    Utilities.sendMessage(type, channel, "Quote " + args[2] + " is: " + quote);
 		else
-		    ((TwitchBot) bot).sendMessage(MJRBot.getTwitchBotByChannelName(channel).getChannel(),
-			    "Quote " + args[1] + " doesnt exist!");
+		    Utilities.sendMessage(type, channel, "Quote " + args[1] + " doesnt exist!");
 	    } else if (args.length > 1 && args[1].equalsIgnoreCase("add")
 		    && Permissions.hasPermission(bot, type, channel, sender, PermissionLevel.Moderator.getName())) {
 		if (!message.contains("@")) {
@@ -81,31 +39,13 @@ public class QuoteCommand extends Command {
 		} else if (message.contains("\"") || message.contains("'")) {
 		    Utilities.sendMessage(type, channel, sender + " your quote must not contain \" or '");
 		} else {
-		    String fileTemp = file.getPath();
-		    Path filePath = Paths.get(fileTemp);
-		    if (!Files.exists(filePath)) {
-			try {
-			    Files.createFile(filePath);
-			} catch (IOException e) {
-			    e.printStackTrace();
-			}
-		    }
-		    message = message.substring(11);
-		    message = "'" + message;
-		    message = message.replace(" @", "' ");
-		    try {
-			Files.write(filePath, ("\n" + message.trim() + " " + Calendar.getInstance().get(Calendar.YEAR) + ";").getBytes(),
-				StandardOpenOption.APPEND);
-		    } catch (IOException e) {
-			e.printStackTrace();
-		    }
+		    QuoteFile.addQuote(channel, file, message);
 		    Utilities.sendMessage(type, channel, sender + " your quote has been added!");
 		    ConsoleUtil.TextToConsole(bot, type, channel,
 			    "A new Quote has been added by " + sender + ". The quote message is " + message, MessageType.Bot, null);
 		}
 	    } else if (args.length == 2 && args[1].equalsIgnoreCase("help")) {
-		((TwitchBot) bot).sendMessage(MJRBot.getTwitchBotByChannelName(channel).getChannel(),
-			"Quote Help: You can use the following commands: !quote or !quote add <message> @<username> or !quote get <number>");
+		Utilities.sendMessage(type, channel, "Quote Help: You can use the following commands: !quote or !quote add <message> @<username> or !quote get <number>");
 	    }
 	}
     }
---
+++ src/com/mjr/files/QuoteFile.java
@@ -0,0 +1,80 @@
+package com.mjr.files;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+@SuppressWarnings("resource") 
+public class QuoteFile {
+
+    public static String getQuote(String channelName, File file, int number) {
+	String token1 = "";
+	Scanner inFile1 = null;
+	try {
+	    inFile1 = new Scanner(file).useDelimiter(";\\s*");
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	}
+	List<String> temps = new ArrayList<String>();
+	while (inFile1.hasNext()) {
+	    token1 = inFile1.next();
+	    temps.add(token1);
+	}
+	inFile1.close();
+	String[] tempsArray = temps.toArray(new String[0]);
+
+	if (number < tempsArray.length)
+	    return tempsArray[number];
+	else
+	    return null;
+    }
+
+    public static String getRandomQuote(String channelName, File file) {
+	String token1 = "";
+	Scanner inFile1 = null;
+	try {
+	    inFile1 = new Scanner(file).useDelimiter(";\\s*");
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	}
+	List<String> temps = new ArrayList<String>();
+	while (inFile1.hasNext()) {
+	    token1 = inFile1.next();
+	    temps.add(token1);
+	}
+	inFile1.close();
+	String[] tempsArray = temps.toArray(new String[0]);
+	Random rand = new Random();
+	return tempsArray[rand.nextInt(tempsArray.length)];
+    }
+
+    public static void addQuote(String channelName, File file, String quote) {
+	String fileTemp = file.getPath();
+	Path filePath = Paths.get(fileTemp);
+	if (!Files.exists(filePath)) {
+	    try {
+		Files.createFile(filePath);
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+	quote = quote.substring(11);
+	quote = "'" + quote;
+	quote = quote.replace(" @", "' ");
+	try {
+	    Files.write(filePath, ("\n" + quote.trim() + " " + Calendar.getInstance().get(Calendar.YEAR) + ";").getBytes(),
+		    StandardOpenOption.APPEND);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+}
