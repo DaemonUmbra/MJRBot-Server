09f0435ba874f1b62f3acee9f537be03dbd58c53
MJRLegends <MJRLegends@users.noreply.github.com>
2018-07-18 02:33:29 +0100

    Made the uptime command work for mixer

--- src/com/mjr/commands/defaultCommands/UptimeCommand.java
+++ src/com/mjr/commands/defaultCommands/UptimeCommand.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import com.mjr.HTTPConnect;
 import com.mjr.MJRBot;
 import com.mjr.MJRBot.BotType;
+import com.mjr.MixerBot;
 import com.mjr.Permissions.PermissionLevel;
 import com.mjr.Utilities;
 import com.mjr.commands.Command;
@@ -19,39 +20,48 @@ public class UptimeCommand extends Command {
 	    String result = HTTPConnect
 		    .GetResponsefrom("https://api.twitch.tv/kraken/streams/" + channel + "?client_id=" + MJRBot.CLIENT_ID);
 	    if (result.contains("updated_at")) {
-		String uptime = result.substring(result.indexOf("updated_at") + 13);
-		uptime = uptime.substring(0, 20);
-
+		String upTime = result.substring(result.indexOf("updated_at") + 13);
+		upTime = upTime.substring(0, 20);
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 		Date parse = null;
 
 		try {
-		    parse = format.parse(uptime);
+		    parse = format.parse(upTime);
 		} catch (ParseException e) {
 		    e.printStackTrace();
 		}
 
-		long currentTime = System.currentTimeMillis();
-
-		Date date = new Date(currentTime);
-
-		long diff = date.getTime() - parse.getTime();
-		long diffDay = diff / (24 * 60 * 60 * 1000);
-		diff = diff - (diffDay * 24 * 60 * 60 * 1000);
-		long diffHours = diff / (60 * 60 * 1000);
-		diff = diff - (diffHours * 60 * 60 * 1000);
-		long diffMinutes = diff / (60 * 1000);
-		diff = diff - (diffMinutes * 60 * 1000);
-		long diffSeconds = diff / 1000;
-		diff = diff - (diffSeconds * 1000);
-
-		Utilities.sendMessage(type, channel,
-			channel + " has been live for " + diffDay + " day(s) " + diffHours + " hour(s) " + diffMinutes + " minute(s)");
+		runCommand(type, channel, sender, parse);
 	    } else {
 		Utilities.sendMessage(type, channel, channel + " is currently not streaming!");
 	    }
-	} else
-	    Utilities.sendMessage(type, channel, "This command isnt available for Mixer, right now sorry!");
+	} else {
+	    MixerBot mixerBot = ((MixerBot) bot);
+	    if(mixerBot.isStreaming())
+		runCommand(type, channel, sender, mixerBot.getUpdatedAt());
+	    else
+		Utilities.sendMessage(type, channel, channel + " is currently not streaming!");
+	}
+    }
+
+    public void runCommand(BotType type, String channel, String sender, Date date2) {
+
+	long currentTime = System.currentTimeMillis();
+
+	Date date = new Date(currentTime);
+
+	long diff = date.getTime() - date2.getTime();
+	long diffDay = diff / (24 * 60 * 60 * 1000);
+	diff = diff - (diffDay * 24 * 60 * 60 * 1000);
+	long diffHours = diff / (60 * 60 * 1000);
+	diff = diff - (diffHours * 60 * 60 * 1000);
+	long diffMinutes = diff / (60 * 1000);
+	diff = diff - (diffMinutes * 60 * 1000);
+	long diffSeconds = diff / 1000;
+	diff = diff - (diffSeconds * 1000);
+
+	Utilities.sendMessage(type, channel,
+		channel + " has been live for " + diffDay + " day(s) " + diffHours + " hour(s) " + diffMinutes + " minute(s)");
     }
 
     @Override
