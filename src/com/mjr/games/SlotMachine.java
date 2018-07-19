19056565de62818b93364623eb620c0bb54daf69
MJRLegends <MJRLegends@users.noreply.github.com>
2018-07-19 16:07:27 +0100

    Fixed Slot machine game showing the wrong Slot 3 result

--- src/com/mjr/games/SlotMachine.java
+++ src/com/mjr/games/SlotMachine.java
@@ -15,10 +15,10 @@ public class SlotMachine {
 	int slot2 = Utilities.getRandom(0, 7);
 	int slot3 = Utilities.getRandom(0, 7);
 	if (type == BotType.Twitch)
-	    return new ResultPair(emotesTwtich[slot1] + " " + emotesTwtich[slot2] + " " + emotesTwtich[slot2],
+	    return new ResultPair(emotesTwtich[slot1] + " " + emotesTwtich[slot2] + " " + emotesTwtich[slot3],
 		    slot1 == slot2 && slot1 == slot3 ? true : false);
 	else
-	    return new ResultPair(emotesMixer[slot1] + " " + emotesMixer[slot2] + " " + emotesMixer[slot2],
+	    return new ResultPair(emotesMixer[slot1] + " " + emotesMixer[slot2] + " " + emotesMixer[slot3],
 		    slot1 == slot2 && slot1 == slot3 ? true : false);
 
     }
