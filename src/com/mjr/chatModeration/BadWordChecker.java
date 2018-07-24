a6acffe5beb2a5a75b62f402a6e56a3bfe77d9d1
MJRLegends <MJRLegends@users.noreply.github.com>
2018-07-24 14:23:24 +0100

    Few tweaks for bad word checker

--- src/com/mjr/chatModeration/BadWordChecker.java
+++ src/com/mjr/chatModeration/BadWordChecker.java
@@ -8,13 +8,12 @@ public class BadWordChecker {
     public static boolean BadwordsBan = false;
 
     public static String[] BadWords = { "Fuck", "Shit", "Cunt", "Wanker", "Tosser", "Slag", "Slut", "Penis", "Cock", "Vagina", "Pussy",
-	    "Boobs", "Tits", "Ass", "Bastard", "Twat", "Nigger", "Bitch", "***", "fuck", "shit", "cunt", "wanker", "tosser", "slag", "slut",
-	    "penis", "cock", "vagina", "pussy", "boobs", "tits", "ass", "bastard", "twat", "nigger", "bitch" };
+	    "Boobs", "Tits", "Ass", "Bastard", "Twat", "Nigger", "Bitch", "***", "Nigga" };
 
     public static void CheckBadWords(Object bot, BotType type, String channelName, String message, String sender) {
 	for (int i = 0; i < BadWords.length; i++) {
-	    String Check = BadWords[i];
-	    if (message.contains(Check)) {
+	    String check = BadWords[i];
+	    if (message.toLowerCase().contains(check.toLowerCase())) {
 		if (!Permissions.hasPermission(bot, type, channelName, sender, PermissionLevel.Moderator.getName())) {
 		    BadwordsBan = true;
 		}
