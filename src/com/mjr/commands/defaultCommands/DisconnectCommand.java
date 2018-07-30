db598d1fe6be5983b2ada48597529e3fd2151977
MJRLegends <MJRLegends@users.noreply.github.com>
2018-07-30 20:58:31 +0100

    Changed disconnect command to fully remove the bot from your channel

--- src/com/mjr/commands/defaultCommands/DisconnectCommand.java
+++ src/com/mjr/commands/defaultCommands/DisconnectCommand.java
@@ -1,26 +1,17 @@
 package com.mjr.commands.defaultCommands;
 
-import com.mjr.MJRBot;
 import com.mjr.MJRBot.BotType;
-import com.mjr.MixerBot;
 import com.mjr.Permissions.PermissionLevel;
-import com.mjr.TwitchBot;
 import com.mjr.commands.Command;
+import com.mjr.sql.MySQLConnection;
 
 public class DisconnectCommand extends Command {
 
     @Override
     public void onCommand(BotType type, Object bot, String channel, String sender, String login, String hostname, String message,
 	    String[] args) {
-	if (type == BotType.Twitch) {
-	    TwitchBot twitchBot = (TwitchBot) bot;
-	    twitchBot.disconnectTwitch();
-	    MJRBot.removeTwitchBot(twitchBot);
-	} else if (type == BotType.Mixer) {
-	    MixerBot mixerBot = (MixerBot) bot;
-	    mixerBot.disconnectMixer();
-	    MJRBot.removeMixerBot(mixerBot);
-	}
+	MySQLConnection.executeUpdate("DELETE from channels where name = " + "\"" + channel.toLowerCase() + "\"" + " AND bot_type" + "\""
+		+ type.getTypeName() + "\"" + ")");
     }
 
     @Override
