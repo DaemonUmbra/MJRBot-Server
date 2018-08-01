b45f021243b9ef6df44498ad162db83e5158b235
MJRLegends <MJRLegends@users.noreply.github.com>
2018-08-01 01:14:15 +0100

    Fixed Auto Points not being disabled when set to 0

--- src/com/mjr/threads/PointsThread.java
+++ src/com/mjr/threads/PointsThread.java
@@ -8,7 +8,6 @@ import com.mjr.files.Config;
 import com.mjr.files.PointsSystem;
 
 public class PointsThread extends Thread {
-    public static long TimeDuration;
     private BotType type;
     private String channelName;
 
@@ -20,42 +19,46 @@ public class PointsThread extends Thread {
 
     @Override
     public void run() {
-	while (type == BotType.Twitch ? MJRBot.getTwitchBotByChannelName(channelName).ConnectedToChannel : MJRBot.getMixerBotByChannelName(channelName).isConnected()) {
+	while (type == BotType.Twitch ? MJRBot.getTwitchBotByChannelName(channelName).ConnectedToChannel
+		: MJRBot.getMixerBotByChannelName(channelName).isConnected()) {
 	    if (Config.getSetting("Points", channelName).equalsIgnoreCase("true")) {
-		TimeDuration = (Integer.parseInt(Config.getSetting("AutoPointsDelay", channelName)) * 60) * 1000;
-		long timenow = System.currentTimeMillis();
-		if (type == BotType.Twitch) {
-		    TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
-		    if (twitchBot.ConnectedToChannel && !twitchBot.viewers.isEmpty() && !twitchBot.viewersJoinedTimes.isEmpty()) {
-			for (int i = 0; i < twitchBot.viewers.size(); i++) {
-			    if (twitchBot.viewersJoinedTimes.containsKey(twitchBot.viewers.get(i))) {
-				long oldtime = twitchBot.viewersJoinedTimes.get(twitchBot.viewers.get(i));
-				if ((timenow - oldtime) >= TimeDuration) {
-				    PointsSystem.AddPoints(twitchBot.viewers.get(i), 1, channelName);
-				    twitchBot.viewersJoinedTimes.put(twitchBot.viewers.get(i), System.currentTimeMillis());
+		int delay = Integer.parseInt(Config.getSetting("AutoPointsDelay", channelName));
+		if (delay != 0) {
+		    long TimeDuration = (delay * 60) * 1000;
+		    long timenow = System.currentTimeMillis();
+		    if (type == BotType.Twitch) {
+			TwitchBot twitchBot = MJRBot.getTwitchBotByChannelName(channelName);
+			if (twitchBot.ConnectedToChannel && !twitchBot.viewers.isEmpty() && !twitchBot.viewersJoinedTimes.isEmpty()) {
+			    for (int i = 0; i < twitchBot.viewers.size(); i++) {
+				if (twitchBot.viewersJoinedTimes.containsKey(twitchBot.viewers.get(i))) {
+				    long oldtime = twitchBot.viewersJoinedTimes.get(twitchBot.viewers.get(i));
+				    if ((timenow - oldtime) >= TimeDuration) {
+					PointsSystem.AddPoints(twitchBot.viewers.get(i), 1, channelName);
+					twitchBot.viewersJoinedTimes.put(twitchBot.viewers.get(i), System.currentTimeMillis());
+				    }
+				}
+			    }
+			}
+		    } else if (type == BotType.Mixer) {
+			MixerBot mixerBot = MJRBot.getMixerBotByChannelName(channelName);
+			if (mixerBot.isConnected() && !mixerBot.getViewers().isEmpty() && !mixerBot.viewersJoinedTimes.isEmpty()) {
+			    for (int i = 0; i < mixerBot.getViewers().size(); i++) {
+				if (mixerBot.viewersJoinedTimes.containsKey(mixerBot.getViewers().get(i))) {
+				    long oldtime = mixerBot.viewersJoinedTimes.get(mixerBot.getViewers().get(i));
+				    if ((timenow - oldtime) >= TimeDuration) {
+					PointsSystem.AddPoints(mixerBot.getViewers().get(i), 1, channelName);
+					mixerBot.viewersJoinedTimes.put(mixerBot.getViewers().get(i), System.currentTimeMillis());
+				    }
 				}
 			    }
 			}
 		    }
-		} else if (type == BotType.Mixer) {
-		    MixerBot mixerBot = MJRBot.getMixerBotByChannelName(channelName);
-		    if (mixerBot.isConnected() && !mixerBot.getViewers().isEmpty() && !mixerBot.viewersJoinedTimes.isEmpty()) {
-			for (int i = 0; i < mixerBot.getViewers().size(); i++) {
-			    if (mixerBot.viewersJoinedTimes.containsKey(mixerBot.getViewers().get(i))) {
-				long oldtime = mixerBot.viewersJoinedTimes.get(mixerBot.getViewers().get(i));
-				if ((timenow - oldtime) >= TimeDuration) {
-				    PointsSystem.AddPoints(mixerBot.getViewers().get(i), 1, channelName);
-				    mixerBot.viewersJoinedTimes.put(mixerBot.getViewers().get(i), System.currentTimeMillis());
-				}
-			    }
-			}
-		    }
-		}
 
-		try {
-		    Thread.sleep(TimeDuration);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
+		    try {
+			Thread.sleep(TimeDuration);
+		    } catch (InterruptedException e) {
+			e.printStackTrace();
+		    }
 		}
 	    } else {
 		try {
