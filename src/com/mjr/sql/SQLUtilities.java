172632284457cc89d625b4105cf3d6853cca7bde
MJRLegends <MJRLegends@users.noreply.github.com>
2018-08-02 18:15:37 +0100

    Tweaked to size of a field in a create table sql command

--- src/com/mjr/sql/SQLUtilities.java
+++ src/com/mjr/sql/SQLUtilities.java
@@ -19,7 +19,7 @@ public class SQLUtilities {
 	MySQLConnection.executeUpdate(
 		"CREATE TABLE mjrbot.quotes (id int(50) not null auto_increment primary key, channel varchar(35), quote varchar(128))");
 	MySQLConnection.executeUpdate(
-		"CREATE TABLE mjrbot.moderation_actions (id int(50) not null auto_increment primary key, channel varchar(35), time varchar(35), user varchar(35), reason varchar(128), message varchar(128))");
+		"CREATE TABLE mjrbot.moderation_actions (id int(50) not null auto_increment primary key, channel varchar(35), time varchar(35), user varchar(35), reason varchar(128), message varchar(500))");
     }
 
     public static boolean doesDataBaseExist(String name) {
