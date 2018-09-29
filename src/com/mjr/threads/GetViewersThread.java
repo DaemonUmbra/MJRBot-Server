89f08904c074bdfea7e02cb8ab1ec79629d7513b
MJRLegends <MJRLegends@users.noreply.github.com>
2018-09-29 02:21:33 +0100

    Fixed getting viewers thread not working

--- src/com/mjr/threads/GetViewersThread.java
+++ src/com/mjr/threads/GetViewersThread.java
@@ -8,21 +8,14 @@ import java.net.URL;
 import com.mjr.ConsoleUtil;
 import com.mjr.ConsoleUtil.MessageType;
 import com.mjr.MJRBot.BotType;
+import com.mjr.TwitchBot;
 import com.mjr.storage.Config;
 import com.mjr.storage.EventLog;
+import com.mjr.storage.EventLog.EventType;
 import com.mjr.storage.PointsSystem;
 import com.mjr.storage.RankSystem;
-import com.mjr.storage.EventLog.EventType;
-import com.mjr.TwitchBot;
 
 public class GetViewersThread extends Thread {
-    public String result = "";
-    public String Viewers = "";
-    public String Mods = "";
-    public String Staff = "";
-    public String Admins = "";
-    public String GMods = "";
-    public String newresult = "";
 
     private TwitchBot bot;
 
@@ -35,13 +28,13 @@ public class GetViewersThread extends Thread {
     public void run() {
 	while (bot.ConnectedToChannel) {
 	    try {
-		result = "";
-		newresult = "";
-		Viewers = "";
-		Mods = "";
-		Staff = "";
-		Admins = "";
-		GMods = "";
+		String result = "";
+		String viewers = "";
+		String moderators = "";
+		String staff = "";
+		String admins = "";
+		String global_moderators = "";
+		String end_result = "";
 		URL url = new URL("https://tmi.twitch.tv/group/user/" + bot.channelName + "/chatters");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setRequestMethod("GET");
@@ -51,78 +44,79 @@ public class GetViewersThread extends Thread {
 		    result += line;
 		}
 		reader.close();
-		if (result.contains("viewers" + "\"" + ": [") || result.contains("moderators" + "\"" + ": [")
-			|| result.contains("staff" + "\"" + ": [") || result.contains("admins" + "\"" + ": [")
-			|| result.contains("global_mods" + "\"" + ": [")) {
+		if (result.contains("moderators" + "\"" + ": [") && !result.contains("moderators" + "\"" + ": [],")) {
+		    moderators = result.substring(result.indexOf("moderators") + 14);
+		    result = moderators;
+		    moderators = moderators.substring(0, moderators.indexOf("],"));
+		}
+		if (result.contains("staff" + "\"" + ": [") && !result.contains("staff" + "\"" + ": [],")) {
+		    staff = result.substring(result.indexOf("staff") + 10);
+		    result = staff;
+		    staff = staff.substring(0, staff.indexOf("],"));
+		}
+		if (result.contains("admins" + "\"" + ": [") && !result.contains("admins" + "\"" + ": [],")) {
+		    admins = result.substring(result.indexOf("admins") + 11);
+		    result = result.substring(result.indexOf("admins") + 11);
+		    admins = admins.substring(0, admins.indexOf("],"));
+		}
+		if (result.contains("global_mods" + "\"" + ": [") && !result.contains("global_mods" + "\"" + ": [],")) {
+		    global_moderators = result.substring(result.indexOf("global_mods") + 16);
+		    result = global_moderators;
+		    global_moderators = global_moderators.substring(0, global_moderators.indexOf("],"));
+		}
+		if (result.contains("viewers" + "\"" + ": [") && !result.contains("viewers" + "\"" + ": []")) {
+		    viewers = result.substring(result.indexOf("viewers") + 12);
+		    result = viewers;
+		    viewers = viewers.substring(0, viewers.indexOf("]"));
+		}
 
-		    if (result.contains("moderators" + "\"" + ": [") && !result.contains("moderators" + "\"" + ": [],")) {
-			Mods = result.substring(result.indexOf("moderators") + 21, result.indexOf("],"));
+		end_result = moderators + staff + admins + global_moderators + viewers;
+		end_result = end_result.replace(" ", "");
+		end_result = end_result.replace("\"", "");
+		if (bot.viewers.isEmpty()) {
+		    for (String viewer : end_result.split(",")) {
+			if (!bot.viewers.contains(viewer.toLowerCase())) {
+			    bot.viewers.add(viewer.toLowerCase());
+			}
 		    }
-		    if (result.contains("staff" + "\"" + ": [") && !result.contains("staff" + "\"" + ": [],")) {
-			Staff = "," + result.substring(result.indexOf("staff") + 16);
-			Staff = Staff.substring(0, Staff.indexOf("],"));
-		    }
-		    if (result.contains("admins" + "\"" + ": [") && !result.contains("admins" + "\"" + ": [],")) {
-			Admins = "," + result.substring(result.indexOf("admins") + 17);
-			Admins = Admins.substring(0, Admins.indexOf("],"));
-		    }
-		    if (result.contains("global_mods" + "\"" + ": [") && !result.contains("global_mods" + "\"" + ": [],")) {
-			GMods = "," + result.substring(result.indexOf("global_mods") + 22);
-			GMods = GMods.substring(0, GMods.indexOf("],"));
-		    }
-		    if (result.contains("viewers" + "\"" + ": [") && !result.contains("viewers" + "\"" + ": []")) {
-			Viewers = "," + result.substring(result.indexOf("viewers") + 18);
-			Viewers = Viewers.substring(0, Viewers.indexOf("]"));
-		    }
+		    ConsoleUtil.TextToConsole(bot, BotType.Twitch, bot.channelName, "Bot has the list of current active viewers!",
+			    MessageType.Bot, null);
 
-		    newresult = Mods + Staff + Admins + GMods + Viewers;
-		    newresult = newresult.replace(" ", "");
-		    newresult = newresult.replace("\"", "");
-		    if (bot.viewers.isEmpty()) {
-			for (String viewer : newresult.split(",")) {
-			    if (!bot.viewers.contains(viewer.toLowerCase())) {
-				bot.viewers.add(viewer.toLowerCase());
-			    }
+		} else {
+		    for (String viewer : end_result.split(",")) {
+			if (!bot.viewers.contains(viewer.toLowerCase())) {
+			    bot.viewers.add(viewer.toLowerCase());
+			    EventLog.addEvent(bot.channelName, viewer, "Joined the channel (Twitch)", EventType.User);
 			}
-			ConsoleUtil.TextToConsole(bot, BotType.Twitch, bot.channelName, "Bot has the list of current active viewers!",
-				MessageType.Bot, null);
-
-		    } else {
-			for (String viewer : newresult.split(",")) {
-			    if (!bot.viewers.contains(viewer.toLowerCase())) {
-				bot.viewers.add(viewer.toLowerCase());
-				EventLog.addEvent(bot.channelName, viewer, "Joined the channel (Twitch)", EventType.User);
-			    }
-			}
-			ConsoleUtil.TextToConsole(bot, BotType.Twitch, bot.channelName, "Bot has updated the list of current active viewers!",
-				MessageType.Bot, null);
-
-			Mods = Mods.replace(" ", "");
-			Mods = Mods.replace("\"", "");
-			for (String mod : Mods.split(",")) {
-			    if (!bot.moderators.contains(mod.toLowerCase())) {
-				bot.moderators.add(mod.toLowerCase());
-			    }
-			}
-			ConsoleUtil.TextToConsole(bot, BotType.Twitch, bot.channelName, "Bot has updated the list of current active moderators!",
-				MessageType.Bot, null);
 		    }
+		    ConsoleUtil.TextToConsole(bot, BotType.Twitch, bot.channelName, "Bot has updated the list of current active viewers!",
+			    MessageType.Bot, null);
 
-		    for (int i = 1; i < bot.viewers.size(); i++) {
-			if (Config.getSetting("Points", bot.channelName).equalsIgnoreCase("true")) {
-			    if (!PointsSystem.isOnList(bot.viewers.get(i), bot.channelName)) {
-				PointsSystem.setPoints(bot.viewers.get(i),
-					Integer.parseInt(Config.getSetting("StartingPoints", bot.channelName)), bot.channelName, false, false);
-			    }
+		    moderators = moderators.replace(" ", "");
+		    moderators = moderators.replace("\"", "");
+		    for (String mod : moderators.split(",")) {
+			if (!bot.moderators.contains(mod.toLowerCase())) {
+			    bot.moderators.add(mod.toLowerCase());
 			}
-			if (Config.getSetting("Ranks", bot.channelName).equalsIgnoreCase("true")) {
-			    if (!RankSystem.isOnList(bot.viewers.get(i), bot.channelName)) {
-				RankSystem.setRank(bot.viewers.get(i), "None", bot.channelName);
-			    }
-			}
-			if (!bot.viewersJoinedTimes.containsKey(bot.viewers.get(i).toLowerCase().toLowerCase()))
-			    bot.viewersJoinedTimes.put(bot.viewers.get(i).toLowerCase().toLowerCase(), System.currentTimeMillis());
 		    }
+		    ConsoleUtil.TextToConsole(bot, BotType.Twitch, bot.channelName,
+			    "Bot has updated the list of current active moderators!", MessageType.Bot, null);
+		}
+
+		for (int i = 1; i < bot.viewers.size(); i++) {
+		    if (Config.getSetting("Points", bot.channelName).equalsIgnoreCase("true")) {
+			if (!PointsSystem.isOnList(bot.viewers.get(i), bot.channelName)) {
+			    PointsSystem.setPoints(bot.viewers.get(i),
+				    Integer.parseInt(Config.getSetting("StartingPoints", bot.channelName)), bot.channelName, false, false);
+			}
+		    }
+		    if (Config.getSetting("Ranks", bot.channelName).equalsIgnoreCase("true")) {
+			if (!RankSystem.isOnList(bot.viewers.get(i), bot.channelName)) {
+			    RankSystem.setRank(bot.viewers.get(i), "None", bot.channelName);
+			}
+		    }
+		    if (!bot.viewersJoinedTimes.containsKey(bot.viewers.get(i).toLowerCase().toLowerCase()))
+			bot.viewersJoinedTimes.put(bot.viewers.get(i).toLowerCase().toLowerCase(), System.currentTimeMillis());
 		}
 	    } catch (Exception e) {
 		e.printStackTrace();
