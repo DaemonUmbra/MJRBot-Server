4027a12fc1bebc3179b68b59340cb0d9389c879b
MJRLegends <MJRLegends@users.noreply.github.com>
2018-12-10 20:58:04 +0000

    Add support for AccountLife command for mixer

--- src/main/java/com/mjr/commands/defaultCommands/AccountLifeCommand.java
+++ src/main/java/com/mjr/commands/defaultCommands/AccountLifeCommand.java
@@ -22,49 +22,56 @@ public class AccountLifeCommand extends Command {
 
 	@Override
 	public void onCommand(BotType type, Object bot, String channel, String sender, String login, String hostname, String message, String[] args) {
-		if (type == BotType.Twitch) {
-			URL url;
-			try {
-				String result = "";
+		URL url;
+		try {
+			String result = "";
+			if (type == BotType.Twitch)
 				url = new URL("https://api.twitch.tv/kraken/users/" + sender.toLowerCase() + "/?client_id=" + MJRBot.CLIENT_ID);
-				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-				connection.setRequestMethod("GET");
-				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-				String line = "";
-				while ((line = reader.readLine()) != null) {
-					result += line;
-				}
-				reader.close();
-				if (result.contains("created_at")) {
-					String time = result.substring(result.indexOf("created_at") + 13);
-					time = time.substring(0, 20);
-
-					SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-					format.setTimeZone(TimeZone.getTimeZone("UTC"));
-					Date parse = null;
-
-					try {
-						parse = format.parse(time);
-					} catch (ParseException e) {
-						MJRBot.logErrorMessage(e);
-					}
-
-					OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
-					Date date = Date.from(utc.toInstant());
-
-					long diffInMilliSec = date.getTime() - parse.getTime();
-					long diffMinutes = TimeUnit.MILLISECONDS.toMinutes(diffInMilliSec) % 60;
-					long diffHours = TimeUnit.MILLISECONDS.toHours(diffInMilliSec) % 24;
-					long diffDays = TimeUnit.MILLISECONDS.toDays(diffInMilliSec) % 365;
-					long diffYears = TimeUnit.MILLISECONDS.toDays(diffInMilliSec) / 365l;
-
-					Utilities.sendMessage(type, channel, "@" + sender + " your twitch account is " + diffYears + " year(s) " + diffDays + " day(s) " + diffHours + " hour(s) " + diffMinutes + " minute(s) old");
-				}
-			} catch (Exception e) {
-				MJRBot.logErrorMessage(e);
+			else
+				url = new URL("https://mixer.com/api/v1/channels/" + sender.toLowerCase());
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				result += line;
 			}
-		} else
-			Utilities.sendMessage(type, channel, "@" + sender + " This command isnt available for Mixer, right now sorry!");
+			reader.close();
+			if (result.contains("created_at") || result.contains("createdAt")) {
+				String time = result.substring(result.contains("created_at") ? (result.indexOf("created_at") + 13) : (result.indexOf("createdAt") + 12));
+				
+				if (type == BotType.Twitch)
+					time = time.substring(0, 20);
+				else
+					time = time.substring(0, 19);
+				SimpleDateFormat format;
+				if (type == BotType.Twitch)
+					format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+				else
+					format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+				format.setTimeZone(TimeZone.getTimeZone("UTC"));
+				Date parse = null;
+
+				try {
+					parse = format.parse(time);
+				} catch (ParseException e) {
+					MJRBot.logErrorMessage(e);
+				}
+
+				OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
+				Date date = Date.from(utc.toInstant());
+
+				long diffInMilliSec = date.getTime() - parse.getTime();
+				long diffMinutes = TimeUnit.MILLISECONDS.toMinutes(diffInMilliSec) % 60;
+				long diffHours = TimeUnit.MILLISECONDS.toHours(diffInMilliSec) % 24;
+				long diffDays = TimeUnit.MILLISECONDS.toDays(diffInMilliSec) % 365;
+				long diffYears = TimeUnit.MILLISECONDS.toDays(diffInMilliSec) / 365l;
+
+				Utilities.sendMessage(type, channel, "@" + sender + " your " + (type == BotType.Twitch ? "twitch" : "mixer") + " account is " + diffYears + " year(s) " + diffDays + " day(s) " + diffHours + " hour(s) " + diffMinutes + " minute(s) old");
+			}
+		} catch (Exception e) {
+			MJRBot.logErrorMessage(e);
+		}
 	}
 
 	@Override
