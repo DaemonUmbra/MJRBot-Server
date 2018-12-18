48fa8f70b7a7dc6ec4af1e103ef35e4cf8baf886
MJRLegends <MJRLegends@users.noreply.github.com>
2018-12-18 02:45:55 +0000

    Added a try catch to catch possible errors from CrossChatLink system

--- src/main/java/com/mjr/CrossChatLink.java
+++ src/main/java/com/mjr/CrossChatLink.java
@@ -13,38 +13,38 @@ import com.mjr.util.Utilities;
 public class CrossChatLink {
 
 	public static void sendMessageAcrossPlatforms(BotType type, String channelName, String sender, String message) {
-		// Exclude commands & bots messages
-		if (message.startsWith("!") || sender.equalsIgnoreCase(ConfigMain.getSetting("TwitchUsername")) || sender.equalsIgnoreCase(ConfigMain.getSetting("MixerUsername/BotName")))
-			return;
-		boolean twitch = type == BotType.Twitch ? false : true;
-		boolean mixer = type == BotType.Mixer ? false : true;
-		boolean discord = Config.getSetting("DiscordEnabled", channelName).equalsIgnoreCase("true") ? true : false;
-		String platformPrefex = null;
-		if(type == BotType.Discord)
-			platformPrefex = "[Discord]";
-		else
-			platformPrefex = type == BotType.Twitch ? "[Twitch]" : "[Mixer]";
-		String senderPrefex = " " + sender + ": ";
-
-		if (twitch && Config.getSetting("TwitchChatLink", channelName).equalsIgnoreCase("true"))
-			Utilities.sendMessage(BotType.Twitch, channelName, platformPrefex + senderPrefex + message);
-		if (mixer && Config.getSetting("MixerChatLink", channelName).equalsIgnoreCase("true"))
-			Utilities.sendMessage(BotType.Mixer, channelName, platformPrefex + senderPrefex + message);
-		if (discord && Config.getSetting("DiscordChatLink", channelName).equalsIgnoreCase("true") && type != BotType.Discord) {
-			if(MJRBot.useManualMode == false) {
-				ResultSet channel_id = MySQLConnection.executeQuery("SELECT cross_link_channel_id FROM discord_info WHERE channel = '" + channelName + "'");
-				try {
+		try {
+			// Exclude commands & bots messages
+			if (message.startsWith("!") || sender.equalsIgnoreCase(ConfigMain.getSetting("TwitchUsername")) || sender.equalsIgnoreCase(ConfigMain.getSetting("MixerUsername/BotName")))
+				return;
+			boolean twitch = type == BotType.Twitch ? false : true;
+			boolean mixer = type == BotType.Mixer ? false : true;
+			boolean discord = Config.getSetting("DiscordEnabled", channelName).equalsIgnoreCase("true") ? true : false;
+			String platformPrefex = null;
+			if(type == BotType.Discord)
+				platformPrefex = "[Discord]";
+			else
+				platformPrefex = type == BotType.Twitch ? "[Twitch]" : "[Mixer]";
+			String senderPrefex = " " + sender + ": ";
+	
+			if (twitch && Config.getSetting("TwitchChatLink", channelName).equalsIgnoreCase("true"))
+				Utilities.sendMessage(BotType.Twitch, channelName, platformPrefex + senderPrefex + message);
+			if (mixer && Config.getSetting("MixerChatLink", channelName).equalsIgnoreCase("true"))
+				Utilities.sendMessage(BotType.Mixer, channelName, platformPrefex + senderPrefex + message);
+			if (discord && Config.getSetting("DiscordChatLink", channelName).equalsIgnoreCase("true") && type != BotType.Discord) {
+				if(MJRBot.useManualMode == false) {
+					ResultSet channel_id = MySQLConnection.executeQuery("SELECT cross_link_channel_id FROM discord_info WHERE channel = '" + channelName + "'");
 					if (channel_id.next()) {
 						if(channel_id.getString("cross_link_channel_id") != null)
 							MJRBot.bot.sendMessage(MJRBot.bot.client.getChannelByID(Long.parseLong(channel_id.getString("cross_link_channel_id"))), platformPrefex + senderPrefex + message);
 					}
-				} catch (NumberFormatException | SQLException e) {
-					MJRBot.logErrorMessage(e);
+				}
+				else {
+					ConsoleUtil.textToConsole("Discord Crosslink is disabled, as it is currently not supported on the file based storage type!");
 				}
 			}
-			else {
-				ConsoleUtil.textToConsole("Discord Crosslink is disabled, as it is currently not supported on the file based storage type!");
-			}
+		} catch (NumberFormatException | SQLException e) {
+			MJRBot.logErrorMessage(e);
 		}
 	}
 }
