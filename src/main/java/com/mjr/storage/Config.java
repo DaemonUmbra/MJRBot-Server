bd938c4d4ea167c0699d1db5989fae05de558fdb
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-18 16:02:27 +0000

    Changed config creation system for database to check if setting doesnt exist and to add it with a default value

--- src/main/java/com/mjr/storage/Config.java
+++ src/main/java/com/mjr/storage/Config.java
@@ -15,60 +15,69 @@ import com.mjr.util.ConsoleUtil;
 public class Config extends FileBase {
 	public static String fileName = "Config.properties";
 
-	public static void loadDefaultsDatabase(String channelName) throws IOException {
+	public static void createSettingIfDoesntExist(String setting, String value, String channelName) {
 		try {
-			if (!MySQLConnection.executeQueryNoOutput("SELECT * FROM config WHERE channel = " + "\"" + channelName + "\"" + " AND setting = " + "\"" + "LinkWarning" + "\"").next()) {
-				setSetting("LinkWarning", "you are not allowed to post links with out permission!", channelName);
-				setSetting("LanguageWarning", "you are not allowed to use that language in the chat!", channelName);
-				setSetting("FollowerMessage", "has followed!", channelName);
-				setSetting("SymbolWarning", "you are using to many symbols", channelName);
-				setSetting("AnnouncementsDelay", "0", channelName);
-				setSetting("GiveawayDelay", "0", channelName);
-				setSetting("StartingPoints", "0", channelName);
-				setSetting("AutoPointsDelay", "0", channelName);
-				setSetting("EmoteWarning", "dont spam emotes!", channelName);
-				setSetting("Commands", "false", channelName);
-				setSetting("Games", "false", channelName);
-				setSetting("Ranks", "false", channelName);
-				setSetting("Points", "false", channelName);
-				setSetting("Announcements", "false", channelName);
-				setSetting("Badwords", "false", channelName);
-				setSetting("LinkChecker", "false", channelName);
-				setSetting("Emote", "false", channelName);
-				setSetting("Symbol", "false", channelName);
-				setSetting("SilentJoin", "true", channelName);
-				setSetting("FollowerCheck", "false", channelName);
-				setSetting("Quotes", "false", channelName);
-				setSetting("MaxSymbols", "5", channelName);
-				setSetting("MaxEmotes", "5", channelName);
-				setSetting("MsgWhenCommandDoesntExist", "true", channelName);
-				setSetting("MsgWhenCommandCantBeUsed", "false", channelName);
-				setSetting("AnnouncementMessage1", "", channelName);
-				setSetting("AnnouncementMessage2", "", channelName);
-				setSetting("AnnouncementMessage3", "", channelName);
-				setSetting("AnnouncementMessage4", "", channelName);
-				setSetting("AnnouncementMessage5", "", channelName);
-				setSetting("CommandsCooldownAmount", "20", channelName);
-				setSetting("SelectedTimeZone", "Europe/London", channelName);
-				setSetting("SubAlerts", "true", channelName);
-				setSetting("ResubAlerts", "true", channelName);
-				setSetting("GiftSubAlerts", "true", channelName);
-				setSetting("HostingAlerts", "true", channelName);
-				setSetting("RaidAlerts", "true", channelName);
-				setSetting("BitsAlerts", "true", channelName);
-				setSetting("FollowAlerts", "true", channelName);
-				setSetting("AnnouncementsWhenOffline", "false", channelName);
-				setSetting("TwitchChatLink", "false", channelName);
-				setSetting("MixerChatLink", "false", channelName);
-				setSetting("DiscordEnabled", "false", channelName);
-				setSetting("DiscordChatLink", "false", channelName);
-				setSetting("AutoPointsWhenOffline", "false", channelName);
-				ResultSet set = MySQLConnection.executeQueryNoOutput("SELECT * FROM badwords WHERE channel = '" + channelName + "'");
-				if (set == null || !set.next()) {
-					String[] badwords = { "Fuck", "Shit", "Cunt", "Wanker", "Tosser", "Slag", "Slut", "Penis", "Cock", "Vagina", "Pussy", "Boobs", "Tits", "Ass", "Bastard", "Twat", "Nigger", "Bitch", "***", "Nigga" };
-					for (int i = 0; i < badwords.length; i++) {
-						MySQLConnection.executeUpdate("INSERT INTO badwords(channel, word) VALUES (" + "\"" + channelName + "\"" + "," + "\"" + badwords[i] + "\"" + ")");
-					}
+			if (!MySQLConnection.executeQueryNoOutput("SELECT * FROM config WHERE channel = " + "\"" + channelName + "\"" + " AND setting = " + "\"" + setting + "\"").next()) {
+				ConsoleUtil.textToConsole("Creating config setting of: " + setting + " with the default value for " + channelName);
+				setSetting(setting, value, channelName);
+			}
+		} catch (SQLException e) {
+			MJRBot.logErrorMessage(e);
+		}
+	}
+	
+	public static void loadDefaultsDatabase(String channelName) throws IOException {
+		createSettingIfDoesntExist("LinkWarning", "you are not allowed to post links with out permission!", channelName);
+		createSettingIfDoesntExist("LanguageWarning", "you are not allowed to use that language in the chat!", channelName);
+		createSettingIfDoesntExist("FollowerMessage", "has followed!", channelName);
+		createSettingIfDoesntExist("SymbolWarning", "you are using to many symbols", channelName);
+		createSettingIfDoesntExist("AnnouncementsDelay", "0", channelName);
+		createSettingIfDoesntExist("GiveawayDelay", "0", channelName);
+		createSettingIfDoesntExist("StartingPoints", "0", channelName);
+		createSettingIfDoesntExist("AutoPointsDelay", "0", channelName);
+		createSettingIfDoesntExist("EmoteWarning", "dont spam emotes!", channelName);
+		createSettingIfDoesntExist("Commands", "false", channelName);
+		createSettingIfDoesntExist("Games", "false", channelName);
+		createSettingIfDoesntExist("Ranks", "false", channelName);
+		createSettingIfDoesntExist("Points", "false", channelName);
+		createSettingIfDoesntExist("Announcements", "false", channelName);
+		createSettingIfDoesntExist("Badwords", "false", channelName);
+		createSettingIfDoesntExist("LinkChecker", "false", channelName);
+		createSettingIfDoesntExist("Emote", "false", channelName);
+		createSettingIfDoesntExist("Symbol", "false", channelName);
+		createSettingIfDoesntExist("SilentJoin", "true", channelName);
+		createSettingIfDoesntExist("FollowerCheck", "false", channelName);
+		createSettingIfDoesntExist("Quotes", "false", channelName);
+		createSettingIfDoesntExist("MaxSymbols", "5", channelName);
+		createSettingIfDoesntExist("MaxEmotes", "5", channelName);
+		createSettingIfDoesntExist("MsgWhenCommandDoesntExist", "true", channelName);
+		createSettingIfDoesntExist("MsgWhenCommandCantBeUsed", "false", channelName);
+		createSettingIfDoesntExist("AnnouncementMessage1", "", channelName);
+		createSettingIfDoesntExist("AnnouncementMessage2", "", channelName);
+		createSettingIfDoesntExist("AnnouncementMessage3", "", channelName);
+		createSettingIfDoesntExist("AnnouncementMessage4", "", channelName);
+		createSettingIfDoesntExist("AnnouncementMessage5", "", channelName);
+		createSettingIfDoesntExist("CommandsCooldownAmount", "20", channelName);
+		createSettingIfDoesntExist("SelectedTimeZone", "Europe/London", channelName);
+		createSettingIfDoesntExist("SubAlerts", "true", channelName);
+		createSettingIfDoesntExist("ResubAlerts", "true", channelName);
+		createSettingIfDoesntExist("GiftSubAlerts", "true", channelName);
+		createSettingIfDoesntExist("HostingAlerts", "true", channelName);
+		createSettingIfDoesntExist("RaidAlerts", "true", channelName);
+		createSettingIfDoesntExist("BitsAlerts", "true", channelName);
+		createSettingIfDoesntExist("FollowAlerts", "true", channelName);
+		createSettingIfDoesntExist("AnnouncementsWhenOffline", "false", channelName);
+		createSettingIfDoesntExist("TwitchChatLink", "false", channelName);
+		createSettingIfDoesntExist("MixerChatLink", "false", channelName);
+		createSettingIfDoesntExist("DiscordEnabled", "false", channelName);
+		createSettingIfDoesntExist("DiscordChatLink", "false", channelName);
+		createSettingIfDoesntExist("AutoPointsWhenOffline", "false", channelName);
+		try {
+		ResultSet set = MySQLConnection.executeQueryNoOutput("SELECT * FROM badwords WHERE channel = '" + channelName + "'");
+			if (set == null || !set.next()) {
+				String[] badwords = { "Fuck", "Shit", "Cunt", "Wanker", "Tosser", "Slag", "Slut", "Penis", "Cock", "Vagina", "Pussy", "Boobs", "Tits", "Ass", "Bastard", "Twat", "Nigger", "Bitch", "***", "Nigga" };
+				for (int i = 0; i < badwords.length; i++) {
+					MySQLConnection.executeUpdate("INSERT INTO badwords(channel, word) VALUES (" + "\"" + channelName + "\"" + "," + "\"" + badwords[i] + "\"" + ")");
 				}
 			}
 		} catch (SQLException e) {
