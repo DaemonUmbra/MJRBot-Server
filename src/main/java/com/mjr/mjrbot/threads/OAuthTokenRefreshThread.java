1f9a23554a8d09df76ae3afee5f42947018a1613
MJRLegends <MJRLegends@users.noreply.github.com>
2019-05-09 15:54:37 +0100

    Tweaks to OAuthTokenRefreshThread

--- src/main/java/com/mjr/mjrbot/threads/OAuthTokenRefreshThread.java
+++ src/main/java/com/mjr/mjrbot/threads/OAuthTokenRefreshThread.java
@@ -8,6 +8,7 @@ import com.mjr.mjrbot.bots.MixerBot;
 import com.mjr.mjrbot.bots.OAuthManager;
 import com.mjr.mjrbot.bots.TwitchBot;
 import com.mjr.mjrbot.storage.sql.MySQLConnection;
+import com.mjr.mjrbot.util.ConsoleUtil;
 import com.mjr.mjrbot.util.HTTPConnect;
 import com.mjr.mjrbot.util.MJRBotUtilities;
 import com.mjr.mjrbot.util.TwitchMixerAPICalls;
@@ -25,24 +26,25 @@ public class OAuthTokenRefreshThread extends Thread {
 				HashMap<Integer, TwitchBot> channelListTwitch = ChatBotManager.getTwitchBots();
 				HashMap<String, MixerBot> channelListMixer = ChatBotManager.getMixerBots();
 
+				ConsoleUtil.textToConsole("Running OAuthTokenRefreshThread");
+
 				for (Integer channelNameMain : channelListTwitch.keySet()) {
-					ResultSet tokenSet = MySQLConnection.executeQuery("SELECT refresh_token FROM tokens WHERE channel_id = '" + channelListTwitch.get(channelNameMain).getChannelID() + "'");
-					if (tokenSet.next() && tokenSet.getString("refresh_token") != null && tokenSet.getString("refresh_token") != "") {
+					ResultSet tokenSet = MySQLConnection.executeQuery("SELECT access_token FROM tokens WHERE channel_id = '" + channelListTwitch.get(channelNameMain).getChannelID() + "'");
+					if (tokenSet.next() && tokenSet.getString("access_token") != null && tokenSet.getString("access_token") != "") {
 						try {
-							HTTPConnect.getRequest(TwitchMixerAPICalls.twitchGetUserAPI(tokenSet.getString("refresh_token")));
+							HTTPConnect.getRequest(TwitchMixerAPICalls.twitchGetUserAPI(tokenSet.getString("access_token")));
 						} catch (Exception e) {
 							if (e.getMessage().contains("401"))
 								OAuthManager.refreshTokenTwitch(1, channelListTwitch.get(channelNameMain));
 						}
-
 					}
 				}
 
 				for (String channelNameMain : channelListMixer.keySet()) {
-					ResultSet tokenSet = MySQLConnection.executeQuery("SELECT refresh_token FROM tokens WHERE channel = '" + channelListMixer.get(channelNameMain).getChannelName() + "' AND platform = 'Mixer'");
-					if (tokenSet.next() && tokenSet.getString("refresh_token") != null && tokenSet.getString("refresh_token") != "") {
+					ResultSet tokenSet = MySQLConnection.executeQuery("SELECT access_token FROM tokens WHERE channel = '" + channelListMixer.get(channelNameMain).getChannelName() + "' AND platform = 'Mixer'");
+					if (tokenSet.next() && tokenSet.getString("access_token") != null && tokenSet.getString("access_token") != "") {
 						HashMap<String, String> headers = new HashMap<String, String>();
-						headers.put("Authorization", "Bearer " + tokenSet.getString("refresh_token"));
+						headers.put("Authorization", "Bearer " + tokenSet.getString("access_token"));
 						try {
 							HTTPConnect.getRequestCustomHeaders(TwitchMixerAPICalls.mixerGetUsersAPI(), headers);
 						} catch (Exception e) {
